46c1e9c1e52342d7bb69e5ce62b3ea4f7c4ab8eb
X00LA <X00LA@users.noreply.github.com>
2021-06-24 21:28:45 +0200

    Added bight vision
    Needs sleeper loop for proper function.

--- client/main.lua
+++ client/main.lua
@@ -1574,3 +1574,36 @@ function ImpoundVehicle(vehicle)
 	ESX.ShowNotification(_U('impound_successful'))
 	currentTask.busy = false
 end
+
+-- Night vision!
+-- TODO
+-- Add sleeper loop
+--[[
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+	end
+
+    PlayerData = ESX.GetPlayerData()
+
+    while PlayerData.job.name == "police" do
+        Citizen.Wait(0)
+        local ped = PlayerPedId()
+        if GetPedPropIndex(ped, 0) == 116 then
+            SetNightvision(true)
+            SetSeethrough(false)
+        elseif GetPedPropIndex(ped, 0) == 118 then
+            SetSeethrough(true)
+            SetNightvision(false)
+        else
+            SetSeethrough(false)
+            SetNightvision(false)
+        end
+    end
+end)
+]]--

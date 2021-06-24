7ef66f13292c7a374a81fab70b1c3c4d801f0da7
X00LA <X00LA@users.noreply.github.com>
2021-06-24 21:21:13 +0200

    Added config for optionalneeds

--- config.lua
+++ config.lua
@@ -9,6 +9,7 @@ Config.EnablePlayerManagement     = false -- Enable if you want society managing
 Config.EnableArmoryManagement     = false
 Config.EnableESXIdentity          = false -- Enable if you're using esx_identity.
 Config.EnableLicenses             = false -- Enable if you're using esx_license.
+Config.EnableESXOptionalneeds 	  = false -- Enable if you're using esx_optionalneeds.
 
 Config.EnableHandcuffTimer        = true -- Enable handcuff timer? will unrestrain player after the time ends.
 Config.HandcuffTimer              = 10 * 60000 -- 10 minutes.

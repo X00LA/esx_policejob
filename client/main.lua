c8ad875674a06e778366cc7c8f57457be2023bd9
X00LA <X00LA@users.noreply.github.com>
2021-06-24 21:32:40 +0200

    Fix: isInService always returning false

--- client/main.lua
+++ client/main.lua
@@ -122,9 +122,8 @@ function OpenCloakroomMenu()
 			local awaitService
 
 			ESX.TriggerServerCallback('esx_service:isInService', function(isInService)
-				if not isInService then
-
-					if Config.MaxInService == -1 then
+				if Config.MaxInService ~= -1 then
+					if not isInService then
 						ESX.TriggerServerCallback('esx_service:enableService', function(canTakeService, maxInService, inServiceCount)
 							if not canTakeService then
 								ESX.ShowNotification(_U('service_max', inServiceCount, maxInService))
@@ -144,24 +143,9 @@ function OpenCloakroomMenu()
 								ESX.ShowNotification(_U('service_in'))
 							end
 						end, 'police')
-					else 
+					else
 						awaitService = true
-						playerInService = true
-
-						local notification = {
-							title    = _U('service_anonunce'),
-							subject  = '',
-							msg      = _U('service_in_announce', GetPlayerName(PlayerId())),
-							iconType = 1
-						}
-
-						TriggerServerEvent('esx_service:notifyAllInService', notification, 'police')
-						TriggerEvent('esx_policejob:updateBlip')
-						ESX.ShowNotification(_U('service_in'))
 					end
-
-				else
-					awaitService = true
 				end
 			end, 'police')
 
